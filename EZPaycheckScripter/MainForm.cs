bc87f1cb46ecf665dfd1d7e0b0caec91e0ab5d05
Timothy Yaukey <yaukey@willowsoft.com>
2019-02-08 19:53:53 -0800

    Fix for 2019.

--- EZPaycheckScripter/MainForm.cs
+++ EZPaycheckScripter/MainForm.cs
@@ -102,7 +102,7 @@ namespace EZPaycheckScripter
                     return;
                 }
                 paycheckCount++;
-                script.AppendLine("Local $window = WinWaitActive(\"ezPayCheck 2018\", \"List Checks After\")");
+                script.AppendLine("Local $window = WinWaitActive(\"ezPayCheck 2019\", \"List Checks After\")");
                 CreatePaycheck(script, check);
             }
 
